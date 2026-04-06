71e9b82a375ffe9a202750af8c193850921a9414
vzbel <142253332+vzbel@users.noreply.github.com>
2026-04-05 20:53:28 -0600

    Add featured section to hero page

---
+++ src/components/FeaturedSection.jsx
@@ -0,0 +1,39 @@
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+
+const FeaturedSection = ({ img }) => {
+  return (
+    <Container sx={{ mt: 5, mb: 5 }}>
+      {/* Header text */}
+      <Box sx={{ mb: 5, textAlign: "center" }} fontFamily="monospace">
+        <Typography
+          variant="h2"
+          sx={{ fontWeight: "bold", fontSize: { xs: "2rem", sm: "2.5rem" } }}
+          fontFamily="inherit"
+        >
+          Display your mood
+        </Typography>
+        <Typography
+          variant="p"
+          sx={{ fontSize: { xs: "1.5rem", sm: "2rem" } }}
+          fontFamily="inherit"
+        >
+          show off your style
+        </Typography>
+      </Box>
+
+      {/* Featured image */}
+      <Box sx={{ height: "400px" }}>
+        <Box
+          component="img"
+          src={img.url}
+          alt={img.alt}
+          sx={{ width: 1, height: 1, objectFit: "cover" }}
+        ></Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default FeaturedSection;
--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,12 +1,19 @@
 import HeroNav from "../components/HeroNav.jsx";
 import HeroSection from "../components/HeroSection.jsx";
 import Box from "@mui/material/Box";
+import FeaturedSection from "../components/FeaturedSection.jsx";
 
 const HeroPage = () => {
+  const featuredImg = {
+    url: "https://i.pinimg.com/1200x/6f/f3/fb/6ff3fbc2e6bb8afb08a9aaf0189a3388.jpg",
+    alt: "Fluid frutiger aero 3D render",
+  };
+
   return (
     <Box>
       <HeroNav />
       <HeroSection />
+      <FeaturedSection img={featuredImg} />
     </Box>
   );
 };
