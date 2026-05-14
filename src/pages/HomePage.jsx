afdc6977e909f05040858ee5563b84ad7215797f
vzbel <142253332+vzbel@users.noreply.github.com>
2026-05-14 09:56:26 -0600

    Feature: Add homepage filters

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,30 +4,61 @@ import PostCard from "../components/home/PostCard.jsx";
 import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
 
+import {
+  Select,
+  MenuItem,
+  FormControlLabel,
+  FormControl,
+  InputLabel,
+  Checkbox,
+} from "@mui/material";
+
 import { useEffect, useState } from "react";
 import { supabase } from "../client.js";
 
 const POSTS_PER_PAGE = 5;
 
+const possibleFilters = ["creation", "upvote", "none"];
+
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(false);
+  const [filters, setFilters] = useState([]);
 
   // Retrieve a few posts
   useEffect(() => {
     let ignore = false;
+    const getUpvotes = async (postId) => {
+      const { count } = await supabase
+        .from("Post_upvotes")
+        .select("*", { count: "exact", head: true })
+        .eq("post_id", postId);
+      return count;
+    };
+
     const getPosts = async () => {
       if (!ignore) {
         const { data, error } = await supabase
           .from("Posts")
           .select()
-          .limit(POSTS_PER_PAGE);
+          .limit(POSTS_PER_PAGE)
+          .order("created_at", { ascending: false });
         if (error) {
           setError(true);
-        } else {
-          setPosts(data);
-          setError(false);
+          return;
         }
+
+        // Retrieve upvote counts for each post
+        const upvoteCounts = {};
+        for (const p of data) {
+          upvoteCounts[p.id] = await getUpvotes(p.id);
+        }
+        const allPosts = data.map((p) => ({
+          ...p,
+          upvotes: upvoteCounts[p.id],
+        }));
+        setPosts(allPosts);
+        setError(false);
       }
     };
     getPosts();
@@ -52,6 +83,29 @@ const HomePage = () => {
     );
   }
 
+  const handleFilterSelect = (e) => {
+    // None cannot be mixed with other filters
+    if (e.target.value.find((el) => el === "none")) {
+      setFilters(["none"]);
+      return;
+    }
+    setFilters(e.target.value);
+  };
+
+  // Apply filters
+  let filteredPosts = [];
+  if (posts.length > 0) {
+    filteredPosts = [...posts];
+    for(const f of filters){
+      // Sort by oldest to newest
+      if(f === "creation"){
+        filteredPosts.sort((p1, p2) => p1.created_at >= p2.created_at);
+      }else if(f === "upvote"){
+        filteredPosts.sort((p1, p2) => p1.upvotes < p2.upvotes);
+      }
+    }
+  }
+
   return (
     <Box>
       <HeroNav />
@@ -65,14 +119,43 @@ const HomePage = () => {
         }}
       >
         {/* Show posts or loading screen */}
-        {posts.length > 0 ? (
+        {filteredPosts.length > 0 ? (
           <>
-            {posts.map((p) => (
+            {/* Filter Controls */}
+            <Box sx={{display: "flex", gap: 3, alignItems: "center", justifyContent: "space-between", width: { xs: "300px", sm: "520px" }, p: 1}}>
+              <Box>
+                <Typography variant="h2" sx={{fontSize: {xs:"1.2rem", sm: "1.5rem"}, mb: 0.5}}>
+                  Posts Home
+                </Typography>
+                <Typography variant="p" sx={{fontSize: {xs: "0.7rem", sm: "0.9rem"}, fontWeight: "200", display: "block"}}>Browsing all posts</Typography>
+              </Box>
+              <FormControl sx={{flex: 1, flexShrink: 1, maxWidth: "150px"}} size="small">
+                <InputLabel id="filter-label">Filter by...</InputLabel>
+                <Select
+                  labelId="filter-label"
+                  label="Filter by..."
+                  value={filters}
+                  onChange={handleFilterSelect}
+                  multiple
+                  sx={{width: "100%"}}
+                >
+                  {possibleFilters.map((f) => (
+                    <MenuItem key={f} value={f}>
+                      {f}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+
+            {filteredPosts.map((p) => (
               <PostCard post={p} key={p.id} />
             ))}
           </>
         ) : (
-          <Typography variant="h5" sx={{mt: 5}}>No posts to show!</Typography>
+          <Typography variant="h5" sx={{ mt: 5 }}>
+            No posts to show!
+          </Typography>
         )}
       </Box>
     </Box>
